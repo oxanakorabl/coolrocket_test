576c17afed92361f49a30dd2004d3d0dedf1a40e
Oksana Skorniakova <oxanakorabl@gmail.com>
2020-05-19 22:30:57 +0300

    add dummy chart

--- app/src/main/java/com/coolrocket/charttest/ui/ChartFragment.kt
+++ app/src/main/java/com/coolrocket/charttest/ui/ChartFragment.kt
@@ -1,11 +1,16 @@
 package com.coolrocket.charttest.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.coolrocket.charttest.R
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import kotlinx.android.synthetic.main.fragment_chart.*
 import me.dmdev.rxpm.base.PmFragment
 
 /**
@@ -22,8 +27,43 @@ class ChartFragment : PmFragment<ChartPm>() {
     }
 
     override fun onBindPresentationModel(pm: ChartPm) {
+
+
+        val entries = ArrayList<Entry>()
+
+
+        entries.add(Entry(1f, 10f))
+        entries.add(Entry(2f, 2f))
+        entries.add(Entry(3f, 7f))
+        entries.add(Entry(4f, 20f))
+        entries.add(Entry(5f, 16f))
+
+
+        val dataSet = LineDataSet(entries, "Points")
+        dataSet.setDrawValues(false)
+        dataSet.setDrawFilled(false)
+        dataSet.setDrawCircleHole(false)
+        dataSet.lineWidth = 3f
+        dataSet.setCircleColor(Color.RED)
+        dataSet.mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+
+
+        lineChart.xAxis.labelRotationAngle = 0f
+
+        lineChart.data = LineData(dataSet)
+        lineChart.axisRight.isEnabled = false
+        lineChart.setTouchEnabled(true)
+        lineChart.setPinchZoom(true)
+
+
+        lineChart.description = null
+
+        lineChart.legendRenderer
+
+
     }
 
     override fun providePresentationModel() = ChartPm()
+
 
 }
