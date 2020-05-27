d80b2692826ed26bf88533e7a9f8d3ced957fd28
Oksana Skorniakova <oxanakorabl@gmail.com>
2020-05-27 14:39:15 +0300

    save chart

--- app/src/main/java/com/coolrocket/charttest/ui/ChartFragment.kt
+++ app/src/main/java/com/coolrocket/charttest/ui/ChartFragment.kt
@@ -1,10 +1,9 @@
 package com.coolrocket.charttest.ui
 
+import android.Manifest
 import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.coolrocket.charttest.R
 import com.coolrocket.charttest.api.Point
@@ -12,15 +11,24 @@ import com.coolrocket.charttest.dagger.ComponentHolder
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.tbruyelle.rxpermissions2.RxPermissions
 import kotlinx.android.synthetic.main.fragment_chart.*
 import me.dmdev.rxpm.base.PmFragment
 import me.dmdev.rxpm.bindTo
+import java.util.*
+
 
 class ChartFragment : PmFragment<ChartPm>() {
 
     private val tableAdapter = TableAdapter()
+    private val router = ComponentHolder.appComponent.getRouter()
 
     override fun providePresentationModel() = ChartPm(ComponentHolder.appComponent.getRepository())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,6 +60,21 @@ class ChartFragment : PmFragment<ChartPm>() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.save -> {
+                askForPermissionForImage()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun setChart(points: List<Point>) {
         val entries = points.map { Entry(it.x, it.y) }
 
@@ -67,6 +90,24 @@ class ChartFragment : PmFragment<ChartPm>() {
 
         chart_fragment_chart.data = LineData(dataSet)
         chart_fragment_chart.invalidate()
+    }
+
+    private fun askForPermissionForImage() {
+        activity?.let {
+            RxPermissions(it)
+                .request(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                .subscribe { granted: Boolean ->
+                    if (granted) {
+                        chart_fragment_chart.saveToGallery("Chart_" + Date().time)
+                        router.showSnack(getString(R.string.chart_fragment_saved))
+                    } else {
+                        router.showSnack(getString(R.string.chart_fragment_no_repmisssion))
+                    }
+                }
+        }
     }
 
 }
